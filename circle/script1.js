c666b0e5d60f4768f935f9aa5daffb88306157d6
久松正太郎 <shotaroazabu@gmail.com>
2017-06-21 09:07:08 +0900

    devide script from HTML

---
+++ circle/script1.js
@@ -0,0 +1,48 @@
+   
+
+    var dataset = [30,30,30,30,30,30,30,30] ;
+    var w1 = 1000 ;
+    var h1 = 100 ;
+
+    var svg1 = d3.select("body").append("svg").attr({width:w1 , height:h1}) 
+    svg1.selectAll("circle")
+       .data(dataset)
+       .enter()
+       .append("circle")
+       .transition()
+       .delay(function(d , i) {
+           return 50 + i * 300
+       })
+       .duration(1000)
+       .ease("bounce")
+       .each("start",function(){
+        d3.select("this")
+          .transition()
+          .attr({
+          cy:h1,
+          r:0,
+          fill:"green"
+          });
+       })
+       .attr({
+        cx:function(d,i){return 50 + (i * 100);},
+        cy:h1 / 2,
+        r: function (d) {return d;} ,
+        fill:"blue"
+       })
+       .each("end",function() {
+         d3.select("this")
+          .transition()
+          .duration(800)
+          .attr({
+            fill:"pink",
+            r:0,
+            cy:0
+          });
+       });
+
+/*    var p1 = d3.select("body");
+    p1.append("p")
+         .text("うんち")
+         .transition()
+         .attr("px","100px")*/
